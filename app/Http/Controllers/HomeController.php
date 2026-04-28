99b393d68a104a6705f19e6ca88c6ba8da2d4e66
nikolastambunan <nikolaskolas325@gmail.com>
2026-04-28 16:09:45 +0700

    NIkolas Tambunan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,36 +3,70 @@
 namespace App\Http\Controllers;
 
 use App\Models\Galeri;
-use App\Models\Berita;
 
 class HomeController extends Controller
 {
-   public function index()
-{
-    $galeri = Galeri::where('status', true)->latest()->take(6)->get();
-    $berita = Berita::with('kategori')->where('status', true)->latest()->take(3)->get();
-    
-    $destinasi = [
-        (object)[
-            'slug' => 'meat',
-            'nama' => 'Meat',
-            'gambar' => '/images/meat/thumbnail.jpg',
-            'deskripsi' => 'Desa adat dengan makam Raja Hunsa dan budaya Batak'
-        ],
-        (object)[
-            'slug' => 'batu-bahisan',
-            'nama' => 'Batu Bahisan',
-            'gambar' => '/images/batu-bahisan/thumbnail.jpg',
-            'deskripsi' => 'Formasi batuan unik dengan spot foto Instagramable'
-        ],
-        (object)[
-            'slug' => 'liang-sipege',
-            'nama' => 'Liang Sipege',
-            'gambar' => '/images/liang-sipege/thumbnail.jpg',
-            'deskripsi' => 'Goa alami dengan stalaktit dan stalakmit'
-        ]
-    ];
-    
-    return view('pages.home', compact('galeri', 'berita', 'destinasi'));
+    public function index()
+    {
+        // DEBUG: Ambil SEMUA data dari tabel galeris dulu
+        $allGaleri = Galeri::where('status', 1)->get();
+        
+        // Ambil slide hero (cari gambar yang mengandung kata 'slide')
+        $slides = Galeri::where('gambar', 'like', '%slide%')
+            ->where('status', 1)
+            ->orderBy('id', 'asc')
+            ->get();
+        
+        // Jika slides kosong, gunakan data dari kategori 'meat'
+        if ($slides->isEmpty()) {
+            $slides = Galeri::where('kategori', 'meat')
+                ->where('status', 1)
+                ->orderBy('id', 'asc')
+                ->take(5)
+                ->get();
+        }
+        
+        // Ambil about image (kategori 'about')
+        $aboutImage = Galeri::where('kategori', 'about')
+            ->where('status', 1)
+            ->first();
+        
+        // Ambil gambar destinasi dari database
+        $destinasiMeat = Galeri::where('kategori', 'meat')
+            ->where('gambar', 'like', '%meat-detail%')
+            ->first();
+        
+        $destinasiBatu = Galeri::where('kategori', 'batu-bahisan')
+            ->where('gambar', 'like', '%batu-detail%')
+            ->first();
+        
+        $destinasiLiang = Galeri::where('kategori', 'liang-sipege')
+            ->where('gambar', 'like', '%liang-detail%')
+            ->first();
+        
+        // Data destinasi
+        $destinasi = [
+            (object)[
+                'slug' => 'meat',
+                'nama' => 'Meat',
+                'gambar' => $destinasiMeat ? $destinasiMeat->gambar : 'image/meat/meat-detail.jpg',
+            ],
+            (object)[
+                'slug' => 'batu-bahisan',
+                'nama' => 'Batu Bahisan',
+                'gambar' => $destinasiBatu ? $destinasiBatu->gambar : 'image/meat/batu-detail.jpg',
+            ],
+            (object)[
+                'slug' => 'liang-sipege',
+                'nama' => 'Liang Sipege',
+                'gambar' => $destinasiLiang ? $destinasiLiang->gambar : 'image/meat/liang-detail.jpg',
+            ],
+        ];
+        
+        // DEBUG: Cek di terminal
+        \Illuminate\Support\Facades\Log::info('Jumlah slide: ' . $slides->count());
+        \Illuminate\Support\Facades\Log::info('Jumlah all galeri: ' . $allGaleri->count());
+        
+        return view('pages.home', compact('slides', 'aboutImage', 'destinasi'));
     }
 }
