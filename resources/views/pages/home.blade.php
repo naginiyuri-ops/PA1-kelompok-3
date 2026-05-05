f039cf4ce4b208aa53b0ac5c98126865b6066a3b
nikolastambunan <nikolaskolas325@gmail.com>
2026-05-05 09:58:31 +0700

    grace

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1286,7 +1286,7 @@
 
     // ==================== SMOOTH SCROLL ====================
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
+        anchor.addEventListener('clic   k', function(e) {
             e.preventDefault();
             const target = document.querySelector(this.getAttribute('href'));
             if (target) {
